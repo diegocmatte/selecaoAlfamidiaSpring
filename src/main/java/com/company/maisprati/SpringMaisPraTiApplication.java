8217d5e4a2711bb0de1729d2b9ec4850185cb1d2
Diego Matté <diegocmatte@gmail.com>
2021-12-28 15:55:02 -0300

    criando projeto

---
+++ src/main/java/com/company/maisprati/SpringMaisPraTiApplication.java
@@ -0,0 +1,13 @@
+package com.company.maisprati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMaisPraTiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMaisPraTiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/maisprati/SpringMaisPraTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.maisprati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMaisPraTiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
